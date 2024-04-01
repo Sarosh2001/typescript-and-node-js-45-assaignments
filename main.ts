f8ac052d993386d2bea5b4aa26e1752095cbede0
Sarosh2001 <160051483+Sarosh2001@users.noreply.github.com>
2024-04-01 17:44:21 +0500

    main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,92 @@
 // //Muhammad Sarosh Nadeem
+//Task 2
+let personName: string = "Eric";
+console.log(`Hello ${personName},would you like to learn some Python today?`);
+
+//Task 3
+
+//Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase, and titlecase.
+
+let personName: string = "Muhammad Sarosh Nadeem";
+
+//in lowercase
+console.log("lowercase:", personName.toLowerCase());
+
+//in upper
+console.log("uppercase:", personName.toUpperCase());
+
+//titlecase
+console.log("titlecase:", personName.replace(/\b\w/g,c=> c.toUpperCase()));
+
+
+//Task 4
+
+//Famous Quote: Find a quote from a famous person you admire. Print the quote and the name of
+//its author. Your output should look something like the following, including the quotation
+//marks:
+
+//Albert Einstein once said, “A person who never made a mistake never tried anything new.”
+
+let quote: string = "A person who never made a mistake never tried anything new.";
+let author: string = "Albert Einstein";
+
+console.log(`${author} once said, "${quote}"`);
+
+//Task 5
+
+//Famous Quote: Find a quote from a famous person you admire. Print the quote and the name of
+//its author. Your output should look something like the following, including the quotation
+//marks:
+
+//Albert Einstein once said, “A person who never made a mistake never tried anything new.”
+
+let quote: string = "A person who never made a mistake never tried anything new.";
+let author: string = "Albert Einstein";
+
+console.log(`${author} once said, "${quote}"`);
+
+
+//Famous Quote 2: Repeat Exercise 4, but this time store the famous person’s name in a variable called famous_person.
+//Then compose your message and store it in a new variable called message. Print your message.
+
+let famous_person: string = "Albert Einstein";
+let message: string = `${famous_person} once said,"${quote}"`;
+
+console.log(message);
+
+
+
+// Task 6
+//Stripping Names: Store a person’s name, and include some whitespace characters at the 
+//beginning and end of the name. Make sure you use each character combination, "\t" and "\n", 
+//at least once. Print the name once, so the whitespace around the name is displayed.
+//Then print the name after striping the white spaces.
+
+let personName: string = "\t\n  Muhammad Sarosh Nadeem  \n\t";
+console.log("Original:", personName);
+console.log("Stripped:", personName.trim());
+
+
+// Task 7 & 8
+//Number Eight: Write addition, subtraction, multiplication, and division operations that each result in the number 8. Be sure to enclose your operations in print statements to see the results.
+
+console.log (5+3);
+console.log (10-2);
+console.log (4*2);
+console.log (16/2);
+
+// Task 9
+//Favorite Number: Store your favorite number in a variable. Then, using that variable, create a message that reveals your favorite number. Print that message.
+
+let favoriteNumber: number = 4;
+console.log (`My favorite number is ${favoriteNumber}.`);
+
+// Task 10
+//Adding Comments: Choose two of the programs you’ve written, and add at least one comment to each. If you don’t have anything specific to write because your programs are too simple at this point, just add your name and the current date at the top of each program file. Then write one sentence describing what the program does.
+
+author: [Muhammad Sarosh Nadeem]
+date: [Thursday, Febrary 22, 2024]
+
 
 // Task 11
 let names : String [] = ["Ali","Haseeb","Wajahat","Muzamil"];
